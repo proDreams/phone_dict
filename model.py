260a0c61113dcdb28df934c41cd670071e590de2
Kosenkov <9588434@gmail.com>
2022-11-12 10:47:02 +0300

    Add model

--- model.py
+++ model.py
@@ -1,1 +1,20 @@
+import csv
+
+
 def view_row():
+    with open('db.csv', 'r') as file:
+        file.readline()
+
+
+def create_row():
+    with open('db.csv', 'a') as file:
+    file.readline()
+
+
+def change_row():
+    with open('db.csv', 'a') as file:
+    file.readline()
+
+
+def create_csv():
+    return 0
